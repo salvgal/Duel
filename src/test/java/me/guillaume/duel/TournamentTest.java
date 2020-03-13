b10737752beaf7de63d5386ae92fd13ac6a0be53
Salvo <sgallo@trilogyed.com>
2020-03-13 18:00:51 +0100

    V2.1-FINAL all test passed

--- src/test/java/me/guillaume/duel/TournamentTest.java
+++ src/test/java/me/guillaume/duel/TournamentTest.java
@@ -82,7 +82,6 @@ public class TournamentTest {
      * once Berserk, he doubles his damages.
      */
     @Test
-    //@Ignore // bonus points when all the rest is neat and tidy
     public void ViciousSwordsmanVsVeteranHighlander() {
 
         Swordsman swordsman = new Swordsman("Vicious")
